f0413341ca73da1535c4991bbfead72d24f1103e
saadman sakib <saadman.sakib2020@gmail.com>
2022-01-16 01:23:40 +0600

    ...

---
+++ src/App.vue
@@ -0,0 +1,121 @@
+<template>
+<nav class="navbar navbar-light navbar-expand-md  d-flex">
+    <div class="container-fluid">
+      <div class="collapse navbar-collapse" id="navcol-1">
+        <ul class="navbar-nav mx-auto expa">
+          <li :class="{ active:is_about_me }"  class="nav-item">
+            <router-link to="/" class="nav-link">About me</router-link>
+          </li>
+          <li :class="{ active:is_skills }" class="nav-item">
+            <router-link  to="/skills" class="nav-link">Skills</router-link>
+          </li>
+           <li :class="{ active:is_education }" class="nav-item">
+            <router-link  to="/education" class="nav-link">Education</router-link>
+          </li>
+          <li class="nav-item"><a class="nav-link" href="#">Projects</a></li>
+          <li class="nav-item"><a class="nav-link" href="#">Experiences</a></li>
+        </ul>
+      </div>
+      <ul class="navbar-nav d-print-none float-none d-md-none d-lg-none d-xl-none flex-row mx-auto colap">
+        <li class="nav-item ">
+          <router-link to="/" class="nav-link"><i class="fa fa-user"></i></router-link>
+        </li>
+        <li class="nav-item ">
+          <router-link to="/skills" class="nav-link"><i class="fa fa-check-square-o"></i></router-link>
+        </li>
+        <li class="nav-item ">
+          <router-link to="/education" class="nav-link"><i class="fa fa-graduation-cap"></i></router-link>
+        </li>
+        <li class="nav-item"><a class="nav-link" href="#"><i class="fa fa-th-list"></i></a></li>
+        <li class="nav-item"><a class="nav-link" href="#"><i class="fa fa-eye"></i></a></li>
+      </ul>
+    </div>
+  </nav>
+  <div class="container">
+    <div class="outside">
+      <div class="inside">
+          <transition name="route" mode="out-in">
+            <router-view :key="$route.fullPath"/>
+          </transition>
+      </div>
+    </div>
+  </div>
+
+  <br><br><br><br>
+  <div class="fab-container">
+    <div class="fab shadow">
+      <div class="fab-content">
+        <span class="material-icons">contacts</span>
+      </div>
+    </div>
+    <div class="sub-button shadow">
+      <a href="https://github.com/saadman-sakib" target="_blank">
+        <span class="material-icons"><i class="fa fa-github" aria-hidden="true"></i></span>
+      </a>
+    </div>
+    <div class="sub-button shadow">
+      <a href="mailto:saadman.sakib@gmail.com" target="_blank">
+        <span class="material-icons">mail</span>
+      </a>
+    </div>
+    <div class="sub-button shadow">
+      <a href="https://facebook.com" target="_blank">
+        <span class="material-icons">facebook</span>
+      </a>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data () {
+    return {
+      message: 'Welcome to Your Vue.js App'
+    }
+  },
+  computed: {
+    is_about_me () {
+      return this.$route.path === '/'
+    },
+    is_skills () {
+      return this.$route.path === '/skills'
+    },
+    is_education () {
+      return this.$route.path === '/education'
+    }
+  }
+}
+</script>
+
+<style>
+.expa > .nav-item > a.router-link-exact-active {
+    color: #8fbcbb !important;
+}
+
+.nav-item > a.router-link-exact-active > i {
+    color: #8fbcbb !important;
+}
+
+.nav-item:has(a.router-link-exact-active){
+    background: #eceff4 ;
+    border-radius: 4px ;
+}
+/* route transitions */
+.route-enter-from {
+  opacity: 0;
+  transform: translateX(50px);
+}
+.route-enter-active {
+  transition: all .1s ease-in;
+}
+
+.route-leave-active {
+  transition: all .1s ease-out;
+}
+.route-leave-to {
+  opacity: 0;
+  transform: translateX(-50px);
+}
+
+</style>
---
+++ src/views/Skills.vue
@@ -0,0 +1,133 @@
+<template>
+<div class="row">
+    <div class="col-sm-12 col-md-12 col-lg-6 p-3">
+    <div class="card">
+        <div class="card-body">
+        <h5 class="card-title">Python</h5>
+        <p class="card-text">
+            Python is a widely used high-level, general-purpose,
+            interpreted, dynamic programming language. Its design
+            philosophy emphasizes code readability, and its syntax allows
+            programmers to express concepts in fewer lines of code than
+            possible in languages such as C++ or Java.
+        </p>
+        <div class="outside">
+            <div class="inside">
+            <img src="../assets/python.png" alt="python" width="130px" />
+            </div>
+        </div>
+        </div>
+    </div>
+    </div>
+
+    <div class="col-sm-12 col-md-12 col-lg-6 p-3">
+    <div class="card">
+        <div class="card-body">
+        <h5 class="card-title">Django</h5>
+        <p class="card-text">
+            Django is a high-level Python Web framework that encourages
+            rapid development and clean, pragmatic design. It encourages
+            developers to write less code, but it also provides a more
+            flexible framework for building applications. Django is
+            designed to help developers build better applications.
+        </p>
+        <div class="outside">
+            <div class="inside">
+            <img src="../assets/django.png" alt="django" width="100px" />
+            </div>
+        </div>
+        </div>
+    </div>
+    </div>
+
+    <div class="col-sm-12 col-md-12 col-lg-6 p-3">
+    <div class="card">
+        <div class="card-body">
+        <h5 class="card-title">Vue JS</h5>
+        <p class="card-text">
+            Vue.js is a progressive framework for building user
+            interfaces. It is a library that lets you build interactive
+            web applications using HTML, CSS, and JavaScript. Vue.js uses
+            the concept of the virtual DOM to efficiently render dynamic
+            UI.
+        </p>
+        <div class="outside">
+            <div class="inside">
+            <img src="../assets/Vuejs.png" alt="vue" width="100px" />
+            </div>
+        </div>
+        </div>
+    </div>
+    </div>
+
+    <div class="col-sm-12 col-md-12 col-lg-6 p-3">
+    <div class="card">
+        <div class="card-body">
+        <h5 class="card-title">Flask</h5>
+        <p class="card-text">
+            Flask is a microframework for Python, originally created by
+            Armin Ronacher in 2007. Flask is designed to be as simple as
+            possible, and it's meant to be. Flask is designed to make it
+            easy to write simple web applications. Flask is designed to be
+            a microframework, not a full-blown framework.
+        </p>
+        <div class="outside">
+            <div class="inside">
+            <img src="../assets/flask.png" alt="flask" width="100px" />
+            </div>
+        </div>
+        </div>
+    </div>
+    </div>
+
+    <div class="col-sm-12 col-md-12 col-lg-6 p-3">
+    <div class="card">
+        <div class="card-body">
+        <h5 class="card-title">Tensorflow</h5>
+        <p class="card-text">
+            TensorFlow is an open source software library for dataflow
+            programming across a range of computing devices. It is
+            developed by Google and is used in a variety of domains
+            including artificial intelligence, machine learning, and
+            scientific computing.
+        </p>
+        <div class="outside">
+            <div class="inside">
+            <img src="../assets/tensorflow.png" alt="tensorflow" width="200px" />
+            </div>
+        </div>
+        </div>
+    </div>
+    </div>
+
+    <div class="col-sm-12 col-md-12 col-lg-6 p-3">
+    <div class="card">
+        <div class="card-body">
+        <h5 class="card-title">Qiskit</h5>
+        <p class="card-text">
+            Qiskit is a software library for quantum computing and quantum
+            information processing (QIP). It is designed to be a
+            general-purpose library for developing quantum computing
+            applications. It is written in Python and is licensed under
+            the Apache License 2.0.
+        </p>
+        <div class="outside">
+            <div class="inside">
+            <img src="../assets/qiskit.png" alt="qiskit" width="100px" />
+            </div>
+        </div>
+        </div>
+    </div>
+    </div>
+</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
